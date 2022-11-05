af858435fb352406098f220dab43828ba22f5091
trankhoa0901085027 <trankhoa0901085027@gmail.com>
2022-11-05 16:01:29 +0700

    update

--- src/main/java/com/r2s/pte/config/SecurityConfig.java
+++ src/main/java/com/r2s/pte/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.r2s.pte.config;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -23,36 +28,57 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors()
-		.and().authorizeRequests().anyRequest().permitAll()
-		.and().csrf().disable();
-		http.addFilterBefore(new CustomAuthorizationFilter(), SecurityContextHolderAwareRequestFilter.class);
+		http.httpBasic().and()
+				.cors()
+				.configurationSource(corsConfigurationSource())
+				.and()
+				.csrf()
+				.disable()
+				.formLogin()
+				.disable();
 	}
+
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	public void configure(WebSecurity web) {
+		web.ignoring().antMatchers("/v2/api-docs",
+				"/configuration/ui",
+				"/swagger-resources/**",
+				"/configuration/security",
+				"/swagger-ui.html",
+				"swagger-ui/index.html",
+				"/webjars/**");
 	}
 
-	@Bean
-	public AccessDeniedHandler accessDeniedHandler() {
-		return new CustomAccessDeniedHandler();
-	}
+
 
 	@Bean
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
-		configuration.addAllowedOrigin("*");
-		configuration.addAllowedHeader("*");
-		configuration.addAllowedMethod("OPTIONS");
-		configuration.addAllowedMethod("HEAD");
-		configuration.addAllowedMethod("GET");
-		configuration.addAllowedMethod("PUT");
-		configuration.addAllowedMethod("POST");
-		configuration.addAllowedMethod("DELETE");
-		configuration.addAllowedMethod("PATCH");
-		configuration.setAllowedOrigins(Arrays.asList("*","http://localhost:3000","https://my.boostpte.com","http://admin.boostpte.com","https://admin.boostpte.com"));
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source;
+		source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+		List<String> all = Collections.singletonList("*");
+		configuration.setAllowedOrigins(all);
+		configuration.setAllowedMethods(all);
+		configuration.setAllowedHeaders(all);
 		source.registerCorsConfiguration("/**", configuration);
-		return new CorsFilter(source);
+		return source;
 	}
+//	@Bean
+//	public CorsFilter corsFilter() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration();
+//		configuration.addAllowedOrigin("*");
+//		configuration.addAllowedHeader("*");
+//		configuration.addAllowedMethod("OPTIONS");
+//		configuration.addAllowedMethod("HEAD");
+//		configuration.addAllowedMethod("GET");
+//		configuration.addAllowedMethod("PUT");
+//		configuration.addAllowedMethod("POST");
+//		configuration.addAllowedMethod("DELETE");
+//		configuration.addAllowedMethod("PATCH");
+//		configuration.setAllowedOrigins(Arrays.asList("*","http://localhost:3000","https://my.boostpte.com","http://admin.boostpte.com","https://admin.boostpte.com"));
+//		source.registerCorsConfiguration("/**", configuration);
+//		return new CorsFilter(source);
+//	}
 }
